49b34eadf1f2825b851eaf9ea612c70190ca7894
Berke Altıparmak <berkealtiparmak@outlook.com>
2022-11-06 11:14:27 +0300

    name-surname-age-gender

--- script.js
+++ script.js
@@ -4,9 +4,23 @@ let userInput = document.getElementById("input")
 let inputSpan = document.getElementById('input-span')
 let clearButton = document.getElementById('clear-button')
 let inputTypeSpan = document.getElementById('input-type-span')
+let nameInput = document.getElementById('name-input')
+let surnameInput = document.getElementById('surname-input')
+let ageInput = document.getElementById('age-input')
+let genderInput = document.getElementById('gender-input')
+let submitNameSurnameAgeGenderButton =  document.getElementById('submit-name-surname-age-gender')
+let nameDisplaySpan = document.getElementById('name-span')
+let surnameDisplaySpan = document.getElementById('surname-span')
+let ageDisplaySpan = document.getElementById('age-span')
+let genderDisplaySpan = document.getElementById('gender-span')
+let errorDisplaySpan = document.getElementById('error-span')
 
-//*************
+
+//************
+//************
 // INPUT BÖLÜMÜ
+//************
+//************
 
 // input'ta her değişiklik olduğunda çalışır. (inputSpan'in değerini girdiğimiz değer yapıyor)
 userInput.oninput = () => { 
@@ -35,3 +49,103 @@ function clearInput() {
 clearButton.addEventListener('click', clearInput)
 
 //************
+//************
+// NAME-SURNAME-AGE-GENDER
+//************
+//************
+
+let personalData = {
+  name: "",
+  surname: "",
+  age: 0,
+  gender: ""
+}
+
+let errorMessage = ""
+
+//bug var bakacağım
+
+nameInput.oninput = () =>{
+  if(nameInput.value != "" && nameInput.value.length < 35){
+    personalData.name = nameInput.value
+  }
+  if (nameInput.value.length < 2 && nameInput.value.length > 35) errorMessage += "  name, "
+}
+surnameInput.oninput = () =>{
+  if(surnameInput.value != "" && surnameInput.value.length < 35){
+    personalData.surname = surnameInput.value
+  }
+  if (surnameInput.value.length < 2 && surnameInput.value.length > 35) errorMessage += "  surname, "
+}
+ageInput.oninput = () =>{
+  if(ageInput.value > 0 && ageInput.value < 125){
+    personalData.age = ageInput.value
+  } else {
+    errorMessage += " age, "
+    personalData.age = 0
+  }
+}
+genderInput.oninput = () =>{
+  let gender = genderInput.value.toLowerCase()
+
+  if(gender != "m" || gender != 'w'){
+    errorMessage += 'gender, '
+  } else {
+    personalData.gender = gender
+  }
+    
+}
+
+function submitNameSurnameAgeGender(){
+  nameDisplaySpan.innerHTML = personalData.name
+  surnameDisplaySpan.innerHTML = personalData.surname
+  ageDisplaySpan.innerHTML = personalData.age
+  genderDisplaySpan.innerHTML = personalData.gender
+  errorDisplaySpan.innerHTML = errorMessage
+}
+
+submitNameSurnameAgeGenderButton.addEventListener('click', submitNameSurnameAgeGender)
+
+//************
+//************
+
+//************
+//************
+
+
+// let vorName = prompt("Bitte schrein Ihre Vorname");
+// let nachName = prompt("Bitte schrein Ihre Nachname");
+// let alter = parseInt(prompt("Bitte schreib Ihre Alt"));
+// let geschlecht = prompt("Bitte schreib Ihre geschelct als Mannlich(M) oder Weiblich(W)").toLowerCase();
+
+// let errormessage = ""
+
+
+
+// if (isNaN(vorName)){
+
+//   if(vorName.length<2 || vorName.length>35){
+//   errormessage +=" Falsche Vorname " 
+//   }
+
+//   if(nachName.length<2 || nachName.length>35){
+//     errormessage +=" Falsche Nachname " 
+//   } 
+
+//   if(alter<0 || alter>125){
+//     errormessage +=" Falsche Alter" 
+//   }
+//   if(geschlecht !="m"||geschlecht !="mannlich"|| geschlecht !="w"||geschlecht !="weiblich"){
+//     errormessage +="Falsche geschelcht"
+//   }
+
+// } else {
+//   alert("vorName string değil")
+// }
+// console.log("Ihre vorName = ",vorName );
+// console.log("Ihre Name = ",nachName );
+// console.log("Ihre Alter = ",alter );
+// console.log("Ihre Geschlecht = ",geschlecht );
+
+// alert(errormessage);
+  
