05421d887c32a283c438ad1a7d359dce13edd9d7
Berke Altıparmak <berkealtiparmak@outlook.com>
2022-12-08 12:33:37 +0300

    express

--- server/server.js
+++ server/server.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const app = express()
-const path = require('path');
-const router = express.Router();
+const path = require('path')
+const bodyParser = require('body-parser')
+const router = express.Router()
 const port = 5005
 
 app.use(express.static("/Users/balpa/Documents/Code/Raw Site/HakanAbi/public/"))
@@ -13,6 +14,11 @@ app.get('/', (req, res) => {
 
 app.get('/workplace', (req, res) => {
   res.sendFile(path.join("/Users/balpa/Documents/Code/Raw Site/HakanAbi/public/index.html"))
+})
+
+app.get('/api/', (req, res) => {
+  res.send('testapi')
+  // pradan dewam xd
 })
 
 
